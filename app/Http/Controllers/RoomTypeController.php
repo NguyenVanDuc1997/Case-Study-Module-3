67b6c8f41cd5797656b9944cc4abd5edb85a0570
NguyenVanDuc1997 <nguyenvanduc160797@gmail.com>
2020-06-11 11:42:47 +0700

    hoan thanh refactor

--- app/Http/Controllers/RoomTypeController.php
+++ app/Http/Controllers/RoomTypeController.php
@@ -22,21 +22,6 @@ class RoomTypeController extends Controller
         return view('layouts.user.rooms.list', compact('roomTypes'));
     }
 
-    public function indexAdminPage(){
-        $roomTypes = $this->roomTypeService->getAll();
-        return view('layouts.admin.room-type.index', compact('roomTypes'));
-    }
-
-    public function editAdminPage($id){
-        $roomType = $this->roomTypeService->getById($id);
-        return view('layouts.admin.room-type.form-edit',compact('roomType'));
-    }
-    public function storeAdminPage(RoomTypeRequest $roomTypeRequest,$id){
-        $roomType = $this->roomTypeService->getById($id);
-        $this->roomTypeService->update($roomTypeRequest, $roomType);
-        return redirect()->route('roomType.index');
-    }
-
     public function getById($id)
     {
         $roomTypes = $this->roomTypeService->getAll();
@@ -45,10 +30,6 @@ class RoomTypeController extends Controller
 
     }
 
-    public function destroyAdminPage($id){
-        $roomType = $this->roomTypeService->getById($id);
-        $this->roomTypeService->destroy($roomType);
-        return redirect()->route('roomType.index');
-    }
+
 
 }
