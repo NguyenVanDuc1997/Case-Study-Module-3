11a8d5d374cb499c86f51657a1b9b8f428bec01d
fire <gioihannaochochungta9x@gmail.com>
2020-06-11 08:56:08 +0700

    change small

--- resources/views/layouts/admin/room/index.blade.php
+++ resources/views/layouts/admin/room/index.blade.php
@@ -4,7 +4,7 @@
         <ol class="breadcrumb mb-4 mt-4">
             <li class="breadcrumb-item"><a href="">Dash</a></li>
             <li class="breadcrumb-item"><a href="">Rooms</a></li>
-            <li class="breadcrumb-item active">Create</li>
+            <li class="breadcrumb-item active">List</li>
         </ol>
         <div class="card mb-4">
             <div class="card-header"><i class="fas fa-table mr-1"></i> <a href="{{route('room.show-form-create')}}" class="btn btn-success">Create</a></div>
